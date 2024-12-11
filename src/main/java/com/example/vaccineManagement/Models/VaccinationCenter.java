0843d994a6c14ec7f55448e91a67322cc28e1aaf
hanannabi <hananjan1234@gmail.com>
2024-12-12 00:10:36 +0530

    first commit

---
+++ src/main/java/com/example/vaccineManagement/Models/VaccinationCenter.java
@@ -0,0 +1,47 @@
+package com.example.vaccineManagement.Models;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "vaccination_center")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VaccinationCenter {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String centreName;
+
+    @DateTimeFormat
+    private LocalTime openingTime;
+
+    private LocalTime closingTime;
+
+    private String address;
+
+    private int doseCapacity;
+
+    @OneToMany(mappedBy = "vaccinationCenter",cascade = CascadeType.ALL)
+    private List<Doctor> doctorList = new ArrayList<>();
+
+}
